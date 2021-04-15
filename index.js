2291680eae97e32507f5ec3aa87fec3a442681c3
Taylor Thornton <thornrae@gmail.com>
2021-04-14 21:43:45 -0700

    some changes testing eb deploy

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let data = {};
+
+app.get('/', (req, res) => {
+  res.status(200).send(data);
+})
+
+app.post('/data', (req, res) => {
+  data = req.body;
+  res.status(201).send('donezo');
+})
+
+app.listen(PORT, () => console.log(`up on port: ${PORT}`));
+
